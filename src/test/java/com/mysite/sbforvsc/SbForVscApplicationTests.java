3d1a92ca1f4dbce7edffa7b3e8edf7a15df55398
dongwookim <donig1225@naver.com>
2024-02-05 09:35:01 +0900

    spring boot project start

---
+++ src/test/java/com/mysite/sbforvsc/SbForVscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbforvsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbForVscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
